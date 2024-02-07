4e8aa2ed477da35a819b89e1c653425d736da203
spartam72dev <symfonydev72@gmail.com>
2024-02-07 20:00:30 +0100

    Création du projet,de la page d'accueil,de l'entité User et ajout de bootstrap

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@popperjs/core';
+import './styles/shared/_home.css';
+import './styles/shared/_navbar.css';
+import './styles/shared/_footer.css';
+import './styles/shared/_carousel.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
